fd999bcf768238ba514596d347a2dc3903c85126
aday-allbase <maxroepz48@gmail.com>
2025-06-07 12:33:30 +0700

    aday

--- scripts/MISC.sh
+++ scripts/MISC.sh
@@ -1,38 +1,28 @@
 #!/bin/bash
 
-# Source the include file containing common functions and variables
-if [[ ! -f "./scripts/INCLUDE.sh" ]]; then
-    echo "ERROR: INCLUDE.sh not found in ./scripts/" >&2
-    exit 1
-fi
-
-set -o errexit  # Exit on error
-set -o nounset  # Exit on unset variables
-set -o pipefail # Exit if any command in a pipe fails
-
 . ./scripts/INCLUDE.sh
 
 # Initialize environment
 init_environment() {
-    log "INFO" "Starting: Downloading misc files and setting up configuration"
-    log "INFO" "Current path: $PWD"
+    log "INFO" "Start Downloading Misc files and setup configuration!"
+    log "INFO" "Current Path: $PWD"
 }
 
 # Setup base-specific configurations
 setup_base_config() {
     # Update date in init settings
     sed -i "s/Ouc3kNF6/${DATE}/g" files/etc/uci-defaults/99-init-settings.sh
-
-    case "${BASE}" in
-        openwrt)
-            log "INFO" "Applying OpenWrt-specific configuration"
+    
+    case "${BASE}}" in
+        "openwrt")
+            log "INFO" "Configuring OpenWrt specific settings"
             sed -i '/# setup misc settings/ a\mv \/www\/luci-static\/resources\/view\/status\/include\/29_temp.js \/www\/luci-static\/resources\/view\/status\/include\/17_temp.js' files/etc/uci-defaults/99-init-settings.sh
             ;;
-        immortalwrt)
-            log "INFO" "Applying ImmortalWrt-specific configuration"
+        "immortalwrt")
+            log "INFO" "Configuring ImmortalWrt specific settings"
             ;;
         *)
-            log "WARN" "Unknown base system: ${BASE}"
+            log "INFO" "Unknown base system: ${BASE}}"
             ;;
     esac
 }
@@ -40,32 +30,36 @@ setup_base_config() {
 # Handle Amlogic-specific files
 handle_amlogic_files() {
     case "${TYPE}" in
-        OPHUB|ULO)
+        "OPHUB")
+            log "INFO" "Removing Amlogic-specific files"
+            rm -f files/etc/uci-defaults/70-rootpt-resize
+            rm -f files/etc/uci-defaults/80-rootfs-resize
+            rm -f files/etc/sysupgrade.conf
+            ;;
+        "ULO")
             log "INFO" "Removing Amlogic-specific files"
             rm -f files/etc/uci-defaults/70-rootpt-resize
             rm -f files/etc/uci-defaults/80-rootfs-resize
             rm -f files/etc/sysupgrade.conf
             ;;
         *)
-            log "INFO" "Non-Amlogic system type: ${TYPE}"
+            log "INFO" "system type: ${TYPE}"
             ;;
     esac
 }
 
 # Setup branch-specific configurations
 setup_branch_config() {
-    local branch_major
-    branch_major=$(echo "${BRANCH}" | cut -d'.' -f1)
-
-    case "${branch_major}" in
-        24)
-            log "INFO" "Applying settings for branch 24.x"
+    local branch_major=$(echo "${BRANCH}" | cut -d'.' -f1)
+    case "$branch_major" in
+        "24")
+            log "INFO" "Configuring for branch 24.x"
             ;;
-        23)
-            log "INFO" "Applying settings for branch 23.x"
+        "23")
+            log "INFO" "Configuring for branch 23.x"
             ;;
         *)
-            log "WARN" "Unknown branch version: ${BRANCH}"
+            log "INFO" "Unknown branch version: ${BRANCH}"
             ;;
     esac
 }
@@ -76,8 +70,8 @@ main() {
     setup_base_config
     handle_amlogic_files
     setup_branch_config
-    log "SUCCESS" "All custom configuration steps completed successfully!"
+    log "SUCCESS" "All custom configuration setup completed!"
 }
 
-# Execute main
+# Execute main function
 main
